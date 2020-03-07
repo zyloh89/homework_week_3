23c7c38b37e0d9c64f30c24701d26677fa4e5ef6
Ziyen <zyloh89@gmail.com>
2020-03-07 19:53:50 +1030

    update script, does not run

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,16 +9,18 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
- 
-  function generatePassword(){
-  alert("Please provide the criteria required for your password");
-  
-// Collect number of characters required for password
-  var length = prompt("How many characters do you require in your password");
-  length = parseInt(length);
 
+var lowercase_code = arrayFromLowToHigh (65, 90)
+var uppercase_code = arrayFromLowToHigh (97, 122)
+var numeric_code = arrayFromLowToHigh (48, 57)
+var specialchar_code = arrayFromLowToHigh (33,47).concat(arrayFromLowToHigh(58, 64))
+
+// Collect number of characters required for password
+  var pwlength = document.getElementById("pwlength").value;
+  pwlength = parseInt(document.getElementById("pwlength"));
+  
 // Check length
-console.log(length);
+
   if (length < 8) {
     alert("Password length too short");
     return;
@@ -26,35 +30,56 @@ console.log(length);
     return;
   }  
 
+  console.log(length);
+
+  
 //Input Form Checkbox for type of characters - Updated HTML
 
 // Validate at least 1 type of character is selected
-alert("Please tick the character types required.");
-var checked = document.checkboxes.lowercase.checked || document.checkboxes.uppercase.checked||document.checkboxes.numeric.checked|| document.checkboxes.specialchar.checked
 
- if (checked == true){
- }
+var uppercase = uppercase.checked
+var lowercase = lowercase.checked
+var numeric = numeric.checked
+var specialchar = specialchar.checked
+function generatePassword(length, lowercase, uppercase, numeric, specialchar){
 
- else {
-   alert ("Please tick at least one character type.")
- }
+    function checked () {
 
+      var checkboxes = document.getElementsByClassName('checkboxes');
+      var okay=false;
+
+      for (var i = 0, l=checkboxes.length; i<l;i++) {
+        if (checkboxes[i].checked) {
+          okay=true;
+          break;
+        }
+
+        else alert("Please tick a box");
+        }
+      }
 }
-console.log(checkboxes)
 
-//put all checkbox in a class
+if (uppercase) charCodes = charCodes.concat(uppercase_code)
+if (numeric) charCodes = charCodes.concat(numeric_code)
+if (specialchar) charCodes = charCodes.concat(specialchar_code)
 
-// id = each checkbox
+var passwordCharacters = []
+for (let i = 0; i < pwlength; i++) {
 
+  var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+  passwordCharacters.push(String.fromCharCode(characterCode))
+}
 
+function arrayFromLowToHigh (low, high) {
 
-
-
-
-// Validate at least 1 type of character is selected
-
-
+  var array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }  
+  return array;
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
+}
